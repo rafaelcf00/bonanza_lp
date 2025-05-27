9369b17b8bc8774686e738290e735f6b33188546
rafaelcf00 <rafacesar0070@gmail.com>
2025-05-26 22:18:51 -0300

    updates

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -22,6 +22,7 @@ const poppins = Poppins({
 export const metadata = {
   title: "Bonanza Fly-in - 2025 - A grande festa!",
   description: "",
+  viewport: 'width=device-width, initial-scale=1',
 };
 
 export default function RootLayout({ children }) {
